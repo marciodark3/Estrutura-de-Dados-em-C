a8ed4b128571baba969cd430a76de103ea654bec
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-10-29 10:50:51 -0300

    Update Selection_Sort_Com_Lista_Dinamica.c

--- Selection_Sort_Com_Lista_Dinamica.c
+++ Selection_Sort_Com_Lista_Dinamica.c
@@ -1,11 +1,40 @@
+/*
+ *@Autor : Marcio Alexandre Freire Sindeaux
+ *@Data : 29/10/2018
+ *
+ *Problema : 
+ *
+ * O RH de uma empresa estava com dificuldades de ultilizar codigos de SQL para ordenar os dados dos funcionários
+ * Diziam que o SQL era dificil de entender(coisa de novato). Em Fim, eles contataram o superior da área, que sabia que
+ * isso poderia ser feito de forma rápida em algumas linguagens.
+ *
+ * Nessa história, o superior da ÁREA fez esse programa.
+ *
+ * Dado algum tempo, o mesmo superior foi fazer entrevistas de emprego para a area de TI(Afinal , ele também é do R.H). 
+ * Nessa entrevista, no qual voce é o entrevistado ele pediu :
+ *
+ * "Faça um programa que implemente um algoritmo de ordenação que não seja o Bubble Sort. Esse programa consiste em um mini
+ *  cadastro de funcionários do RH. Implemente funções dinamicas, afinal, voce não sabe quantos empregados existem nessa empresa.
+ *  Obs : EU TE GARANTO QUE NENHUM DADO SERÁ COLOCADO DE FORMA ERRADA, portanto não precisa sem preocupar em remover os itens."
+ *
+ * Faça o que seu futuro chefe te pede.
+ *
+ *RESOLUÇÃO:
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
+#include<bits/stdc++.h>
 
 typedef struct dadosPessoas{
+	/*
+	 * Estrutura nde os dados dos funcionários vão ficar
+	 */
     char nome[100];
 	int cpf;
 	int matricula;
 }dadosPessoas;
+
 typedef struct pessoa{
 	struct dadosPessoas dados;
 	struct pessoa * proximo;
@@ -24,7 +53,6 @@ void ordenaMatricula(mainList * l);
 void ordenaCPF(mainList * l);
 void buscaPessoa(mainList * l);
 void inserePessoa(mainList * l);
-void retiraPessoa(mainList * l);
 void mostrarLista(mainList * l);
 
 
@@ -34,10 +62,10 @@ int main(void){
 	mainList * l = alocaLista();
 	while(opc != 0){
 		printf("\n\n[1] - Inserir Pessoa no sistema\n");
-		printf("[2] - Retirar Pessoa\n");
-		printf("[3] - Buscar Pessoa\n");
-		printf("[4] - Mostrar pessoas em ordem por CPF\n");
-		printf("[5] - Mostrar pessoas em ordem por matricula\n");
+		printf("[2] - Buscar Pessoa\n");
+		printf("[3] - Mostrar pessoas em ordem por CPF\n");
+		printf("[4] - Mostrar pessoas em ordem por matricula\n");
+		printf("[5] - Mostrar Lista \n");
 		printf("[0] - Sair\n");
 		scanf("%d",&opc);
 		switch(opc){
@@ -47,15 +75,12 @@ int main(void){
 				inserePessoa(l);
 			break;
 			case 2:
-				retiraPessoa(l);
-			break;
-			case 3:
 				buscaPessoa(l);
 			break;
-			case 4:
+			case 3:
 				ordenaCPF(l);
 			break;
-			case 5:
+			case 4:
 				ordenaMatricula(l);
 			break;
             case 10:
@@ -69,7 +94,6 @@ int main(void){
 }
 
 mainList * alocaLista(){
-    //Funciona
     mainList * L = (mainList*)malloc(sizeof(mainList));
     L->qtd = 0;
     L->inicio = NULL;
@@ -78,14 +102,12 @@ mainList * alocaLista(){
 }
 
 pessoa * alocaPessoa(){
-    //Funciona
     pessoa * p = (pessoa*)malloc(sizeof(pessoa));
     p->proximo = NULL;
     p->anterior = NULL;
     return(p);
 }
 void ordenaMatricula(mainList * l){
-    //Funciona
     if(l->inicio == NULL){
         printf("\nNenhum item cadastrado\n");
     }else{
@@ -113,7 +135,6 @@ void ordenaMatricula(mainList * l){
 }
 
 void ordenaCPF(mainList * l){
-    //Funciona
     if(l->inicio == NULL){
         printf("\nNenhum item cadastrado\n");
     }else{
@@ -173,11 +194,10 @@ void buscaPessoa(mainList * l){
 }
 
 void inserePessoa(mainList * l){
-    //Funciona !!
     pessoa * p = alocaPessoa();
     printf("\n\nDigite o nome da Pessoa : \t");
-    scanf("%s",NULL);
-    scanf("%[^\n]s",p->dados.nome);
+    fflush(stdin);
+    fgets(p->dados.nome,100,stdin);
     printf("\nDigite apenas os numeros do CPF da pessoa : \t");
     scanf("%d",&p->dados.cpf);
     printf("\nDigite a numeração da matricula da p0essoa na empresa : \t");
@@ -194,44 +214,11 @@ void inserePessoa(mainList * l){
         l->qtd++;
     }   
 }
-void retiraPessoa(mainList * l){
-    long int mat;
-    printf("\n\nDIGITE A MATRICULA DA PESSOA QUE VAI SER RETIRADA \t:\t");
-    scanf("%ld",&mat);
-    pessoa * aux = l->inicio;
-    if(aux->dados.matricula == mat){
-        l->inicio = aux->proximo;
-        l->qtd--;
-    }else {
-        while(aux->proximo != NULL ){
-            if(aux != l->fim){
-                if(aux->dados.matricula == mat){
-                    pessoa * antaux = aux->anterior;
-                    pessoa * proxaux = aux->proximo;
-                    antaux->proximo = proxaux;
-                    proxaux->anterior = antaux;
-                    l->qtd--;
-                    break;
-                }
-            }else{
-                if(aux->dados.matricula == mat){
-                    l->fim = aux->anterior;
-                    l->qtd--;
-                    break;
-                }
-            }
-                
-            aux = aux->proximo;
-        }
-    }    
-    free(aux);
-}
 
 void mostrarLista(mainList * l){
-    //funciona
     pessoa * aux = l->inicio;
     while(aux != NULL ){
-        printf("\nPessoa %s Encontrada \n",aux->dados.nome);
+        printf("\nPessoa %s",aux->dados.nome);
         printf("Matricula : %ld",aux->dados.matricula);
         printf("\nCPF : %ld\n\n",aux->dados.cpf);
         printf("--------------------------------------------");
